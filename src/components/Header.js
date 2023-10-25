b1d4716d62a7c9680eba6f9c1108624d8fd87e0b
Brian Babu <briansmkenya@gmail.com>
2023-10-25 22:46:44 +0300

    done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 function Header(props) {
-      return (
-        <h1>
-          {/* uncomment the line below to pass the test */}
-          {/* hello from the Header! */}
-        </h1>
-      )
+  return (
+    <header>
+      <h1>
+        {/* uncomment the line below to pass the test */}
+        hello from the Header!
+      </h1>
+      <p>Hello world</p>
+    </header>
+  )
 }
 
 export default Header;
